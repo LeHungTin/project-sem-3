f91cf742896f2d54f7694b159540d64f57560f02
HQQuang <huyquang.nhq@gmail.com>
2019-08-26 23:51:20 +0700

    Create Project

---
+++ ProjectQTSem3/ProjectQT.DataModel/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectQT.DataModel.Models
+{
+    public class Product : BaseEntity
+    {
+        public int CategoryId { get; set; }
+        public string ProduceCode { get; set; }
+        public string Name { get; set; }
+        public long Price { get; set; }
+        public long SalePrice { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime UpdateAt { get; set; }
+        public string UpdateBy { get; set; }
+
+        public Order Orders { get; set; }
+        public ProductAttribute ProductAttributes { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Categories { get; set; }
+    }
+}
---
+++ ProjectQTSem3/ProjectQT.Web/Controllers/accountController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectQT.Web.Controllers
+{
+    public class accountController : Controller
+    {
+        // GET: account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Action Register Method GET
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        ///// <summary>
+        ///// Action Register Method POST
+        ///// </summary>
+        ///// <returns></returns>
+        //[HttpPost ]
+        //public ActionResult Register()
+        //{
+
+        //}
+
+    }
+}
